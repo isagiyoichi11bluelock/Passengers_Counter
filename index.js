672fa3f2477ea116b2f47766dd073bb8f7fac79e
isagiyoichi <agrawalsatyam0123@gmail.com>
2025-10-25 19:31:29 +0530

    final_piece

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let countEl= document.getElementById("count-el")
+let saveEl= document.getElementById("save-el")
 let count= 0
 
 function increment() {
-    count= count + 1
+    count += 1
     countEl.innerText= count
 }
-
+ 
 function save() {
-    console.log(count)
+    let countStr= count + " - " 
+    saveEl.textContent += countStr
+    countEl.innerText= 0
 }
